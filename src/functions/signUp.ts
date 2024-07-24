acfa6ad11890198d11ff28e205cf7c83c9955ee1
lueberGandra <lueber_emp@hotmail.com>
2024-07-24 06:52:32 -0300

    feat: implements signup error handling

--- src/functions/signUp.ts
+++ src/functions/signUp.ts
@@ -2,20 +2,18 @@ import type { APIGatewayProxyEventV2 } from 'aws-lambda';
 import { } from '@aws-sdk/client-cognito-identity-provider';
 
 import { bodyParser } from '@/utils/bodyParser';
-import { response } from '@/utils/response';
 import { signUp } from '@/libs/cognitoClient';
 
 export async function handler(event: APIGatewayProxyEventV2) {
   try {
     const body = bodyParser(event.body ?? '');
-    const userExternalId = await signUp({
-      firstName:body.firstName,
-      lastName:body.lastName,
-      Password:body.password,
-      Username:body.email
+    return await signUp({
+      firstName: body.firstName,
+      lastName: body.lastName,
+      Password: body.password,
+      Username: body.email
     });
-    
-    return response(201, { userExternalId });
+
   } catch (error) {
     throw new Error('Signup failed!');
   }
--- src/libs/cognitoClient.ts
+++ src/libs/cognitoClient.ts
@@ -1,10 +1,11 @@
-import { CognitoIdentityProviderClient, SignUpCommand } from '@aws-sdk/client-cognito-identity-provider';
+import { response } from '@/utils/response';
+import { CognitoIdentityProviderClient, SignUpCommand, UsernameExistsException } from '@aws-sdk/client-cognito-identity-provider';
 
 type ISignUp = {
-    Password: string, Username: string, firstName: string, lastName: string
+  Password: string, Username: string, firstName: string, lastName: string
 }
-export const signUp = async ({ Password, Username, firstName, lastName }: ISignUp):Promise<string|undefined> => {
-  try {    
+export const signUp = async ({ Password, Username, firstName, lastName }: ISignUp): Promise<any> => {
+  try {
     const cognitoClient = new CognitoIdentityProviderClient({});
     const command = new SignUpCommand({
       ClientId: process.env.COGNITO_CLIENT_ID,
@@ -17,10 +18,13 @@ export const signUp = async ({ Password, Username, firstName, lastName }: ISignU
       }
       ]
     });
-    const {UserSub} = await cognitoClient.send(command);
-    return UserSub;
+    const { UserSub } = await cognitoClient.send(command);
+    return response(201, { userExternalId: UserSub });
   } catch (error) {
-    throw new Error('Cognito signup failed!');
+    if (error instanceof UsernameExistsException) {
+      return response(409, { error: 'This email is already in use.' });
+    }
+    return response(500, { error: 'Internal server error.' });
   }
 };
 
