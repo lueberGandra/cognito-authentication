4f26ee44f0a57c2340560a946ee125fb307a4f3b
lueberGandra <lueber_emp@hotmail.com>
2024-07-27 12:03:02 -0300

    feat: integrates signIn with cognito

--- src/libs/cognitoClient.ts
+++ src/libs/cognitoClient.ts
@@ -1,5 +1,5 @@
 import { response } from '@/utils/response';
-import { CognitoIdentityProviderClient, ConfirmSignUpCommand, SignUpCommand, UsernameExistsException } from '@aws-sdk/client-cognito-identity-provider';
+import { CognitoIdentityProviderClient, ConfirmSignUpCommand, InitiateAuthCommand, SignUpCommand, UsernameExistsException, UserNotConfirmedException, UserNotFoundException } from '@aws-sdk/client-cognito-identity-provider';
 
 const ClientId = process.env.COGNITO_CLIENT_ID;
 
@@ -44,6 +44,39 @@ export const accountConfirmation = async ({ ConfirmationCode, Username }: IAccou
     await cognitoClient.send(command);
     return response(204);
   } catch (error) {
+    return response(500, { error: 'Internal server error.' });
+  }
+};
+
+type ISignIn = {
+  Password: string, Username: string
+}
+
+export const signIn = async ({ Password, Username }: ISignIn): Promise<any> => {
+  try {
+    const cognitoClient = new CognitoIdentityProviderClient({});
+    const command = new InitiateAuthCommand({
+      ClientId,
+      AuthFlow: 'USER_PASSWORD_AUTH',
+      AuthParameters: {
+        Username, Password
+      }
+    });
+    const { AuthenticationResult } = await cognitoClient.send(command);
+
+    if (!AuthenticationResult) {
+      return response(401, { response: 'Invalid credentials.' });
+    }
+    return response(201, { response: AuthenticationResult });
+  } catch (error) {
+    if (error instanceof UserNotFoundException) {
+      return response(401, { response: 'Invalid credentials.' });
+    }
+
+    if (error instanceof UserNotConfirmedException) {
+      return response(401, { response: 'You need to confirm your account before signin.' });
+    }
+
     return response(500, { error: 'Internal server error.' });
   }
 };
